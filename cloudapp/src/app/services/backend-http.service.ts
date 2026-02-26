9266ba6385624d6de8fb869d2613e1baec859a69
v1lling <sascha.villing@web.de>
2026-02-26 14:01:14 +0100

    Remove production URL placeholder in BackendHttpService for future configuration

--- cloudapp/src/app/services/backend-http.service.ts
+++ cloudapp/src/app/services/backend-http.service.ts
@@ -15,7 +15,7 @@ import { finalize, map, shareReplay, switchMap, take, tap } from 'rxjs/operators
 	providedIn: 'root',
 })
 export class BackendHttpService {
-	private static readonly PROD_URL = 'https://staff.swisscovery.network';
+	private static readonly PROD_URL = ''; // TODO: set when production backend is ready
 	private static readonly SANDBOX_URL = 'https://staff.swisscovery.network'; // TODO: change when test backend is ready
 	private static readonly LOCAL_URL = 'http://localhost:8080';
 
