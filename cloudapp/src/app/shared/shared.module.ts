d55b4054a05c96f2f66e07e86385689e71404023
v1lling <sascha.villing@web.de>
2026-04-22 11:34:21 +0200

    Add edit schedule and library codes tooltip components; update localization for scheduling features

--- cloudapp/src/app/shared/shared.module.ts
+++ cloudapp/src/app/shared/shared.module.ts
@@ -1,21 +1,28 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from '@exlibris/exl-cloudapp-angular-lib';
 import { TranslateModule } from '@ngx-translate/core';
 
 import { ConfirmDialogComponent } from './components/confirm-dialog/confirm-dialog.component';
+import { EditScheduleDialogComponent } from './components/edit-schedule-dialog/edit-schedule-dialog.component';
+import { LibraryCodesTooltipComponent } from './components/library-codes-tooltip/library-codes-tooltip.component';
 import { LinkStatusTooltipComponent } from './components/link-status-tooltip/link-status-tooltip.component';
 import { UserTypeChipComponent } from './components/user-type-chip/user-type-chip.component';
 
 @NgModule({
 	declarations: [
 		ConfirmDialogComponent,
+		EditScheduleDialogComponent,
+		LibraryCodesTooltipComponent,
 		LinkStatusTooltipComponent,
 		UserTypeChipComponent,
 	],
-	imports: [CommonModule, MaterialModule, TranslateModule],
+	imports: [CommonModule, MaterialModule, ReactiveFormsModule, TranslateModule],
 	exports: [
 		ConfirmDialogComponent,
+		EditScheduleDialogComponent,
+		LibraryCodesTooltipComponent,
 		LinkStatusTooltipComponent,
 		UserTypeChipComponent,
 		TranslateModule,
